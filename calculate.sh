579f917283feb7e802709132a1620ff455e8dca9
Peter Ruzanov <pruzanov@gmail.com>
2019-09-25 16:24:18 -0400

    Custom calculate script modified

--- calculate.sh
+++ calculate.sh
@@ -4,9 +4,8 @@ set -o errexit
 set -o pipefail
 
 cd $1
-
-qrsh -l h_vmem=8G -cwd -now n "\
-module load samtools/1.9 2>/dev/null; \
+module load samtools/0.1.19 2>/dev/null
+qrsh -V -l h_vmem=8G -cwd -now n "\
 find . -regex '.*\.bam$' \
--exec sh -c \" samtools flagstat {} | tr '\n' '\t'; echo \" \; \
+       -exec sh -c \" samtools flagstat {} | tr '\n' '\t'; echo \" \; \
 | sort | uniq | tr '\t' '\n'"
