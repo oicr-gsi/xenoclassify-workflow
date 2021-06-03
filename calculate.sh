f356ac0d4d6f38c50d05e8eee20b5edbe983628e
pruzanov <pruzanov@oicr.on.ca>
2021-06-03 16:14:56 -0400

    Adjusted calculated.sh script

--- calculate.sh
+++ calculate.sh
@@ -4,5 +4,4 @@ set -o errexit
 set -o pipefail
 
 module load samtools/0.1.19 2>/dev/null
-#qrsh -V -l h_vmem=8G -cwd -now n "find . -regex '.*\.bam$' -exec sh -c \" samtools flagstat {} | tr '\n' '\t'; echo \" \; | sort | uniq | tr '\t' '\n'"
-find . -regex '.*\.bam$' -exec sh -c \" samtools flagstat {} | tr '\n' '\t'; echo \" \; | sort | uniq | tr '\t' '\n'
+find . -regex '.*\.bam$' | tail -n 1 | xargs samtools flagstat  | tr '\t' '\n'; echo " " | sort | uniq | tr '\t' '\n'
