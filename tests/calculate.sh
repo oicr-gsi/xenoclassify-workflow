589d3c95189c3a359d08f83198f218ae925fb972
pruzanov <pruzanov@oicr.on.ca>
2022-01-22 12:42:39 -0500

    Minor change to RT script

--- tests/calculate.sh
+++ tests/calculate.sh
@@ -9,6 +9,9 @@ samtools view ./Xenoclassify_filtered.bam | md5sum
 
 # For json file we do the md5sum
 find . -name *.json | xargs md5sum
+JUNCTIONS="./Xenoclassify.Chimeric.out.junction"
 # Optional files from STAR
-find . -name '*.ReadsPerGene.out.tab' | xargs md5sum
-find . -name '*Chimeric.out.junction' | xargs md5sum
+if [[ -f $JUNCTIONS ]];then
+  find . -name '*.ReadsPerGene.out.tab' | xargs md5sum
+  find . -name '*Chimeric.out.junction' | xargs md5sum
+fi
