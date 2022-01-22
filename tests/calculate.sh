aee9ce1ea5c9ab935784cf787f54b8a0acde78da
pruzanov <pruzanov@oicr.on.ca>
2022-01-22 12:12:10 -0500

    Minor change to RT script

--- tests/calculate.sh
+++ tests/calculate.sh
@@ -5,7 +5,7 @@ set -o pipefail
 
 # For bam file we do the md5sum
 module load samtools/1.9
-samtools view Xenoclassify_filtered.bam | md5sum
+samtools view ./Xenoclassify_filtered.bam | md5sum
 
 # For json file we do the md5sum
 find . -name *.json | xargs md5sum
